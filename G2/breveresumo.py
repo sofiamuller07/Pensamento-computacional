e7e9981b8b67dc779c1efb9136eb935db72590f3
sofiamuller07 <sofiamuller2007@gmail.com>
2026-06-25 20:26:12 -0300

    Update breveresumo.py

--- G2/breveresumo.py
+++ G2/breveresumo.py
@@ -1,1 +1,5 @@
-
+A ideia do projeto foi criar uma lixeira inteligente que abre a tampa automaticamente quando uma pessoa aproxima a mão, utilizando Arduino, um sensor ultrassônico e um micro servo. 
+A inspiração veio de vídeos que assisti no TikTok, e achei interessante transformar essa ideia em um projeto para a faculdade.
+Durante o desenvolvimento, montei o circuito, fiz a programação e realizei vários testes até que a abertura automática funcionasse corretamente.
+Também tentei adicionar LEDs para indicar o nível da lixeira, mas não consegui fazer essa parte funcionar.
+No final, o projeto atendeu ao objetivo principal, oferecendo uma solução simples, prática e mais higiênica para o descarte de resíduos. 
